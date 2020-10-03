f5ddc30854fe98669737aadebef38476896ad8a4
Winston Spencer Sonnesen <sonnesen@gmail.com>
2020-10-03 15:17:59 -0300

    Curso Android parte 1: Crie sua primeira app mobile - aula 2

--- app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -2,15 +2,25 @@ package br.com.alura.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Hello Android World!", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "José"));
+
+        ListView alunosView = findViewById(R.id.activity_main_lista_de_alunos);
+        alunosView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
     }
 }
