61674bde3a9ca8dca78c31cd875d2730d08a5a33
Winston Spencer Sonnesen <sonnesen@gmail.com>
2020-10-03 21:11:35 -0300

    Curso Android parte 1: Crie sua primeira app mobile - aula 4

--- app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,26 +1,29 @@
-package br.com.alura.agenda;
+package br.com.alura.agenda.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+import br.com.alura.agenda.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
+        setTitle("Lista de alunos");
 
         List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "José", "Ana"));
 
-        ListView alunosView = findViewById(R.id.activity_main_lista_de_alunos);
+        ListView alunosView = findViewById(R.id.activity_lista_alunos_listview);
         alunosView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
     }
 }
